baf97597c962b3f79bd30723b24faf4ee887e2c2
Ulistonee <ulistonee@gmail.com>
2024-11-30 16:24:58 +0500

    feat: add location of current link to header and burger menu

--- christmas-shop/pages/scripts/burger.js
+++ christmas-shop/pages/scripts/burger.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     const burger = document.getElementById('burger');
     const burgerMenu = document.getElementById('burgerMenu');
-    const links = burgerMenu.querySelectorAll('a');
+    const headerMenu = document.getElementById('headerMenu');
+    const headerLinks = headerMenu.querySelectorAll('li');
+    const mobileLinks = burgerMenu.querySelectorAll('li');
+    const currentUrl = window.location.href;
 
     burger.addEventListener('click', () => {
         burger.classList.toggle('active');
@@ -9,11 +12,41 @@ document.addEventListener('DOMContentLoaded', () => {
         document.body.classList.add('no-scroll');
     });
 
-    links.forEach(link => {
-        link.addEventListener('click', () => {
+    headerLinks.forEach((li) => {
+        const link = li.querySelector('a');
+        console.log(link)
+        if (link && currentUrl.includes(link.getAttribute('href'))) {
+            li.classList.add('non-interactive');
+        }
+    });
+
+    mobileLinks.forEach((li) => {
+        const link = li.querySelector('a');
+        if (link && currentUrl.includes(link.getAttribute('href'))) {
+            li.classList.add('non-interactive');
+        }
+    });
+
+    headerMenu.addEventListener('click', function (event) {
+        headerLinks.forEach((li) => {
+            li.classList.remove('non-interactive');
+        })
+        const clickedLi = event.target.closest('li');
+        if (clickedLi) {
+            clickedLi.classList.add('non-interactive');
+        }
+    })
+
+    burgerMenu.addEventListener('click', function (event) {
+        mobileLinks.forEach((li) => {
+            li.classList.remove('non-interactive');
+        })
+        const clickedLi = event.target.closest('li');
+        if (clickedLi) {
+            clickedLi.classList.add('non-interactive');
             burger.classList.remove('active');
             burgerMenu.classList.remove('active');
             document.body.classList.remove('no-scroll');
-        });
-    });
+        }
+    })
 });
